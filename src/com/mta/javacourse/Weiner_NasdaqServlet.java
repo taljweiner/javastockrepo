bd67e01c704841d1199f098d1af732e633a72026
Tal_Weiner <Tal_Weiner@Tal-LP>
2015-04-05 20:38:18 +0300

    Ex 02

--- src/com/mta/javacourse/Weiner_NasdaqServlet.java
+++ src/com/mta/javacourse/Weiner_NasdaqServlet.java
@@ -5,7 +5,13 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Weiner_NasdaqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1 , num2 , num3 ;
+		num1 = 4 ;
+		num2 = 3 ;
+		num3 = 7 ;
+		int result = ( num1 + num2 ) * num3 ;
+		String resultStr = new String("<h1>Result of " + "(" + num1 + "+"  + num2 + ")" + "*" + num3 + "=" + result +  "</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
